1a5c8e52cf56734a5a33cac92875563ebe2738f8
Bruno Dorigon <brunodorigon@outlook.com>
2025-03-14 15:08:54 -0300

    build route in input

--- src/components/formulario/Form.tsx
+++ src/components/formulario/Form.tsx
@@ -1,11 +1,29 @@
+import { FormEvent, useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router";
 
 const Form = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+    navigate(`/detail/${input}`);
+  }
+
   return (
-    <form className="flex justify-center w-full max-w-xl m-auto gap-6">
+    <form
+      className="flex justify-center w-full max-w-xl m-auto gap-6"
+      onSubmit={handleSubmit}
+    >
       <input
         type="text"
         className="bg-white rounded-md w-full h-10 border-none p-2 outline-none"
+        placeholder="Digite o nome da moeda Ex: Bitcoin"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button type="submit" className="cursor-pointer bg-transparent">
         <BsSearch size={30} color="#fff" />
