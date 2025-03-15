99099de4c42a213fe82329bc932b46fe9652d5d8
Bruno Dorigon <brunodorigon@outlook.com>
2025-03-14 21:02:35 -0300

    change requisition

--- src/components/tabela/Tabela.tsx
+++ src/components/tabela/Tabela.tsx
@@ -1,6 +1,67 @@
+import { useState, useEffect } from "react";
+
 import { Link } from "react-router";
 
+type coinProps = {
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  explorer: string;
+  volumeUsd24Hr: string;
+  formatedPrice?: string;
+  formatedMarke?: string;
+  formatedVolume?: string;
+};
+
+type DataProps = {
+  data: coinProps[];
+};
+
 const Tabela = () => {
+  const [coins, setCoins] = useState<coinProps[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+      .then((resp) => resp.json())
+      .then((data: DataProps) => {
+        const dataResult = data.data;
+
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+        const priceCompact = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+          notation: "compact",
+        });
+
+        const result = dataResult.map((item) => {
+          const resultFormated = {
+            ...item,
+            formatedPrice: price.format(+item.priceUsd),
+            formatedMarket: priceCompact.format(+item.marketCapUsd),
+            formatedVolume: priceCompact.format(+item.volumeUsd24Hr),
+          };
+          return resultFormated;
+        });
+
+        setCoins(result);
+      });
+  }
+
   return (
     <table className="border-spacing-x-4 mt-6 max-w-6xl m-auto table-fixed w-full">
       <thead>
@@ -24,46 +85,53 @@ const Tabela = () => {
       </thead>
 
       <tbody>
-        <tr className="bg-gray-700 text-sm md:text-md lg:text-lg border-b-1 border-solid md:border-gray-100 text-white">
-          <td
-            className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
-            data-label="Moeda"
-          >
-            <div>
-              <Link to="/detail/bitcoin">
-                <span>Bitcoin </span> | BTC
-              </Link>
-            </div>
-          </td>
+        {coins.length > 0
+          ? coins.map((item) => (
+              <tr
+                className="bg-gray-700 text-sm md:text-md lg:text-lg border-b-1 border-solid md:border-gray-100 text-white"
+                key={item.id}
+              >
+                <td
+                  className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
+                  data-label="Moeda"
+                >
+                  <div>
+                    <Link to="/detail/bitcoin">
+                      <span>Bitcoin </span> | BTC
+                    </Link>
+                  </div>
+                </td>
 
-          <td
-            className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
-            data-label="valor mercado"
-          >
-            1T
-          </td>
+                <td
+                  className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
+                  data-label="valor mercado"
+                >
+                  1T
+                </td>
 
-          <td
-            className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
-            data-label="preco"
-          >
-            8.000
-          </td>
+                <td
+                  className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
+                  data-label="preco"
+                >
+                  8.000
+                </td>
 
-          <td
-            className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
-            data-label="Volume"
-          >
-            2B
-          </td>
+                <td
+                  className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full border-b-2 border-gray-100 md:border-none"
+                  data-label="Volume"
+                >
+                  2B
+                </td>
 
-          <td
-            className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full"
-            data-label="Mudança 24hrs"
-          >
-            <span>1.20</span>
-          </td>
-        </tr>
+                <td
+                  className="p-3 text-center font-bold flex justify-between items-center md:table-cell relative before:content-[attr(data-label)] before:text-gray-400 md:before:content-none before:text-sm w-full"
+                  data-label="Mudança 24hrs"
+                >
+                  <span>1.20</span>
+                </td>
+              </tr>
+            ))
+          : null}
       </tbody>
     </table>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,58 +1,8 @@
-import { useState, useEffect } from "react";
 import Tabela from "../../components/tabela/Tabela";
 import Form from "../../components/formulario/Form";
 import ButtonCarregar from "../../components/buttonCarregar/ButtonCarregar";
 
-type coinProps = {
-  id: string;
-  name: string;
-  symbol: string;
-  priceUsd: string;
-  vwap24Hr: string;
-  changePercent24Hr: string;
-  rank: string;
-  supply: string;
-  maxSupply: string;
-  marketCapUsd: string;
-  explorer: string;
-};
-
-type DataProps = {
-  data: coinProps[];
-};
-
 const Home = () => {
-  const [coins, setCoins] = useState<coinProps[]>([]);
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  async function getData() {
-    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
-      .then((resp) => resp.json())
-      .then((data: DataProps) => {
-        const dataResult = data.data;
-
-        const price = Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        });
-
-        const result = dataResult.map((item) => {
-          const resultFormated = {
-            ...item,
-            price: price.format(+item.priceUsd),
-          };
-
-          return resultFormated;
-        });
-
-        console.log(result);
-        return result;
-      });
-  }
-
   return (
     <main className="bg-gray-800 w-full h-screen px-2">
       <Form />
