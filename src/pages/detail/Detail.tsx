bb594676c929526153569be215aee282dd19eb3a
Bruno Dorigon <brunodorigon@outlook.com>
2025-03-15 18:38:45 -0300

    change rotate img

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -17,8 +17,11 @@ const Detail = () => {
   const navigate = useNavigate();
 
   const [coin, setCoin] = useState<coinProps>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
+
     async function getCoin() {
       try {
         fetch(`https://api.coincap.io/v2/assets/${id}`)
@@ -48,6 +51,7 @@ const Detail = () => {
             };
 
             setCoin(dataFormated);
+            setLoading(false);
           });
       } catch (error) {
         console.log(error);
@@ -58,7 +62,65 @@ const Detail = () => {
     getCoin();
   }, [id, navigate]);
 
-  return <div>{coin && <div>{coin.name}</div>}</div>;
+  if (loading) {
+    return <h4 className="text-4xl text-center mt-4">Carregando Detalhes</h4>;
+  }
+  return (
+    <div className="w-full h-screen bg-gray-800">
+      {coin && (
+        <div className="flex flex-col items-center px-2">
+          <h1 className="mb-4 text-xl md:text-2xl md:my-4 text-white font-bold">
+            {coin.name} {coin.symbol}
+          </h1>
+
+          <div className="bg-gray-600 md:mt-3 flex flex-col p-3 gap-2 w-full max-w-sm rounded-md text-white">
+            <img
+              src={`https://assets.coincap.io/assets/icons/${coin.symbol.toLowerCase()}@2x.png`}
+              alt="logo da criptomoeda"
+              className="block object-cover w-12 h-12 self-center hover:rotate-360 transition duration-700 ease-in-out"
+            />
+            <span className="block text-center text-md font-bold md:text-3xl">
+              {coin.name} | {coin.symbol}
+            </span>
+
+            <span className="font-bold text-lg md:text-xl text-center">
+              Preço:
+              <span className="text-sm md:text-md ml-3">
+                {coin.formatedPrice}
+              </span>
+            </span>
+
+            <span className="font-bold tex-md md:text-xl text-center">
+              Mercado:
+              <span className="text-sm md:text-md ml-3">
+                {coin.formatedMarket}
+              </span>
+            </span>
+
+            <span className="font-bold text-lg md:text-xl text-center">
+              Volume:
+              <span className="text-sm md:text-md ml-3">
+                {coin.formatedVolume}
+              </span>
+            </span>
+
+            <span className="font-bold text-lg md:text-xl text-center">
+              Mudança 24hr:
+              <span
+                className={` ${"text-sm md:text-xl ml-3"} ${
+                  +coin.changePercent24Hr > 0
+                    ? "text-green-400"
+                    : "text-red-600"
+                }`}
+              >
+                {Number(coin.changePercent24Hr).toFixed(3)}
+              </span>
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
